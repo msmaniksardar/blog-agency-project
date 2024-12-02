75c2b2d83587df14806d1988ce9a018cfa02e8d5
Manik Sardar <anonymousmanik@gmail.com>
2024-12-02 14:51:12 +0600

    start project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-orange-500">Home Page</h1>
+        </div>
+    );
+};
+
+export default App;
